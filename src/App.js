f3502bec649abca944b45a89200c25d89e299131
AhmtDnmz42 <ahmt.dnmz99@gmail.com>
2023-07-17 17:58:13 +0300

    ilk rc

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      Test
+      <img src="/logo192.png" alt="" />
+      <img src={logo} alt="" />
+    </div>
+  );
+}
+
+export default App;
